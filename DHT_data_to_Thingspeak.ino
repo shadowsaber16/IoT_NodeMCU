48bdd2dfbef090599cd3a2725ea15c714e5abdd3
shadowsaber16 <eshan.gupta@students.iiit.ac.in>
2020-09-14 22:00:34 +0530

    Code

---
+++ DHT_data_to_Thingspeak.ino
@@ -0,0 +1,76 @@
+#include <DHT.h> 
+
+#include <ESP8266WiFi.h>
+ 
+String apiKey = "MOKAJ6DSBFBQ1H9A";   
+
+const char *ssid =  "chiranjeev@5net";  
+const char *pass =  "ANUgoyal12@";
+const char* server = "api.thingspeak.com";
+
+#define DHTPIN 0         
+ 
+DHT dht(DHTPIN, DHT11);
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(115200);
+       delay(10);
+       dht.begin();
+ 
+       Serial.println("Connecting to ");
+       Serial.println(ssid);
+ 
+ 
+       WiFi.begin(ssid, pass);
+ 
+      while (WiFi.status() != WL_CONNECTED) 
+     {
+            delay(500);
+            Serial.print(".");
+     }
+      Serial.println("");
+      Serial.println("WiFi connected");
+ 
+}
+ 
+void loop() 
+{
+  
+       float t = dht.readTemperature();
+      float h = dht.readHumidity();
+     
+      
+             
+                         if (client.connect(server,80))  
+                      {  
+                            
+                             String postStr = apiKey;
+                             postStr +="&field1=";
+                             postStr += String(t);
+                             postStr +="&field2=";
+                             postStr += String(h);
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");  
+                             client.print(postStr);
+ 
+                             Serial.print("Temperature: ");
+                             Serial.print(t);
+                             Serial.print(" degrees Celcius, Humidity: ");
+                             Serial.print(h);
+                        }
+          client.stop();
+  
+          
+  delay(5000);
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+The additional libraries used in the code can be found in the repository IoT_Libraries.
+The code can be copied to the Arduino Folder and along with the specified Libraries in 
+the Libraries folder and directly dumped to the NodeMCU.
+ 
